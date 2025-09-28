f600c1a3e47775163f2a8e16c3d9b4cac83d13bd
Deepika <147088829+Deepika253@users.noreply.github.com>
2025-09-28 21:28:38 +0530

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -6,7 +6,7 @@ export const products = [
     category: "electronics",
     price: 99.99,
     image:
-      "https://images.unsplash.com/photo-1580894894516-c4dc23831bf0?auto=format&fit=crop&w=400&q=80",
+      "https://plus.unsplash.com/premium_photo-1679513691474-73102089c117?q=80&w=800&auto=format&fit=crop",
   },
   {
     id: "2",
@@ -15,7 +15,7 @@ export const products = [
     category: "electronics",
     price: 149.99,
     image:
-      "https://images.unsplash.com/photo-1519125323398-675f0ddb6308?auto=format&fit=crop&w=400&q=80",
+      "https://images.unsplash.com/photo-1660844817855-3ecc7ef21f12?q=80&w=400&auto=format&fit=crop",
   },
   {
     id: "3",
@@ -24,7 +24,7 @@ export const products = [
     category: "fashion",
     price: 75.0,
     image:
-      "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?auto=format&fit=crop&w=400&q=80",
+      "https://images.unsplash.com/photo-1479219136056-56bb6495a005?q=80&w=800&auto=format&fit=crop",
   },
   {
     id: "4",
@@ -33,7 +33,7 @@ export const products = [
     category: "fashion",
     price: 120.0,
     image:
-      "https://images.unsplash.com/photo-1528701800489-4764a6f14411?auto=format&fit=crop&w=400&q=80",
+      "https://plus.unsplash.com/premium_photo-1672046218369-67e12ed1c364?q=80&w=800&auto=format&fit=crop",
   },
   {
     id: "5",
@@ -42,6 +42,6 @@ export const products = [
     category: "home",
     price: 55.99,
     image:
-      "https://images.unsplash.com/photo-1512427691650-471a7f94d6c7?auto=format&fit=crop&w=400&q=80",
+      "https://images.unsplash.com/photo-1608354580875-30bd4168b351?q=80&w=800&auto=format&fit=crop",
   },
 ];
